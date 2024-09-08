36dc67210525a441c1b70f1b126d407b0b63fc61
taylan.kara <taylan.ekin.kara@reateknoloji.com>
2024-09-08 16:36:39 +0300

    vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,12 +9,7 @@ export default defineConfig({
     host: "::",
     port: "8080",
   },
-  plugins: [
-    svgr({
-      exportAsDefault: true,  // Varsayılan olarak SVG'yi export etmesini sağlıyoruz
-    }),
-    react()
-  ],
+  plugins: [react()],
   resolve: {
     alias: [
       {
@@ -28,4 +22,8 @@ export default defineConfig({
       },
     ],
   },
+  build: {
+    outDir: "dist",  // Üretilen dosyaların yer alacağı klasör
+  },
+  base: "/",  // Statik dosyalar için temel URL, eğer Cloudflare veya Nginx altında "/assets/" gibi bir yol kullanıyorsanız ayarlayabilirsiniz.
 });
